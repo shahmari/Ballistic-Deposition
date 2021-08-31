a1f5eec6d01ce2c9f25352fe47de0ecf0ed62dba
shahmari <shahmari.acer@outlook.com>
2021-08-31 16:30:36 +0430

    BDWR updated, functions added.

--- Deposition/Ballistic-Deposition-with-Relaxation/Relaxing-Ballistic-Deposition.jl
+++ Deposition/Ballistic-Deposition-with-Relaxation/Relaxing-Ballistic-Deposition.jl
@@ -1,54 +1,46 @@
 using Plots
 using Statistics
 
-surf = [[] for i=1:202]
-VarList = []
-for n in 1:100
-    randsurf = rand(2:201,1000)
-    for i in randsurf
-        if i == 2 && length(surf[i]) >= length(surf[i-1]) - 2
-            append!(surf[i-1], n%10)
-        elseif i == 201 && length(surf[i]) >= length(surf[i+1]) - 2
-            append!(surf[i+1], n%10)
-        end
-        len = Dict(length(surf[i-1])=>i-1,
-            length(surf[i])=>i,
-            length(surf[i+1])=>i+1,
-            )
-        minlen = min(length(surf[i-1]),length(surf[i]),length(surf[i+1]))
-        append!(surf[len[minlen]], n%10)
-    end
-    SurfInTime = []
+function Leveling(surface)
+    maxlen = 0
+    surf = surface
     for i in surf
-        append!(SurfInTime, length(i))
+        if length(i) > maxlen
+            maxlen = length(i)
+        end
     end
-    append!(VarList,std(SurfInTime))
+    for i in surf
+        append!(i, [3 for i=1:maxlen - length(i)])
+    end
+    return surf
 end
 
-maxlen = 0
-for i in surf
-    if length(i) > maxlen
-        maxlen = length(i)
+function Deposition_2DG(len, tot_time, rate, color_step)
+    surf = [[] for i=1:len + 2]
+    for n in 0:tot_time
+        randsurf = rand(2:len + 1, rate)
+        for i in randsurf
+            if i == 2 && length(surf[i]) >= length(surf[i-1]) - 2
+                append!(surf[i-1], n%10)
+            elseif i == len + 1 && length(surf[i]) >= length(surf[i+1]) - 2
+                append!(surf[i+1], n%10)
+            end
+            lens_ = Dict(
+                length(surf[i-1])=>i-1,
+                length(surf[i])=>i,
+                length(surf[i+1])=>i+1,
+                    )
+            minlen = min(length(surf[i-1]),length(surf[i]),length(surf[i+1]))
+            append!(surf[lens_[minlen]], n%10)
+        end
     end
-end
-
-for i in surf
-    append!(i, [1 for i=1:maxlen - length(i)])
+    return Leveling(surf)
 end
 
 theme(:dark)
 gr()
-heatmap(hcat(surf...), c = :solar, legend = false, border=:none)
-savefig("C:\\Users\\Yaghoub\\Desktop\\Deposition\\Ballistic-Deposition-with-Relaxation\\deposition.png")
 
-scatter(VarList,
-    xscale=:log,
-    yscale=:log,
-    ylims = (0,5),
-    xticks=(1:20:101, 0:20:100),
-    yticks=(1:1:5, 0:1:5),
-    xlabel= "Time",
-    ylabel= "W",
-    title= "Log plot of W-Time",
-    legend = nothing)
-savefig("C:\\Users\\Yaghoub\\Desktop\\Deposition\\Ballistic-Deposition-with-Relaxation\\log-scale.png")
+surf = Deposition_2DG(200, 100, 1000, 10)
+
+heatmap(hcat(surf...), c = :solar, legend = false)
+savefig("C:\\Users\\Yaghoub\\Documents\\GitHub\\Ballistic-Deposition\\Deposition\\Ballistic-Deposition-with-Relaxation\\Fig\\deposition.png")
