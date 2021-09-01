78dc2f5032c47dd7a82747ec9e491158335b859c
shahmari <shahmari.acer@outlook.com>
2021-09-02 00:57:58 +0430

    statical improvement.

--- Deposition/Ballistic-Deposition-with-Relaxation/Relaxing-Ballistic-Deposition-log.jl
+++ Deposition/Ballistic-Deposition-with-Relaxation/Relaxing-Ballistic-Deposition-log.jl
@@ -5,7 +5,7 @@ function Deposition(;len, tot_time, time_steps)
     surf = [0 for i=1:len+2]
     VarList = [0.0 for i=1:time_steps]
     for n in 1:time_steps
-        randsurf = rand(2:len+1,floor(Int,Time[n]*50 + exp(tot_time)/2))
+        randsurf = rand(2:len+1,floor(Int,Time[n] + 200*exp(tot_time)))
         for i in randsurf
             if i == 2 && surf[i] >= surf[i-1] - 2
                 surf[i-1] += 1
@@ -42,23 +42,23 @@ function FindLine(tot_steps, Time, VarList)
         Line = Linear_fit(;Paraline...)
         absl = abs(Line[1]*Time[i] + Line[2] - VarList[i])
         standev = std(Line[1].*Time[2:i] .+ Line[2] .- VarList[2:i])
-        if absl > 1.2 && standev > 1.5 && retline == [0.0 0.0]
+        if absl > 1 && standev > 1 && retline == [0.0 0.0]
             retline = Line
         end
-        if absl > 3 && standev > 2.7
+        if absl > 1.5 && standev > 2.5
             return retline, i
         end
     end
 end
-
+#=
 Parameters = Dict(
-                :len => 300,
-                :tot_time => 12,
+                :len => 200,
+                :tot_time => 8,
                 :time_steps => 100)
 Time, VarList = Deposition(;Parameters...)
 
-#theme(:dark)
-#gr()
+theme(:dark)
+gr()
 
 scatter(log.(Time),log.(VarList),
     xlabel= L"Log\ Time",
@@ -72,4 +72,29 @@ X = 0:log(Time[last_point])
 Y = X .* Line[1] .+ Line[2]
 plot!(X,Y,label = L"y = %$(round(Line[1],digits= 2))x + %$(round(Line[2],digits= 2))")
 
-#savefig("C:\\Users\\Yaghoub\\Documents\\GitHub\\Ballistic-Deposition\\Deposition\\Ballistic-Deposition-with-Relaxation\\Fig\\log-log.png")
+savefig("C:\\Users\\Yaghoub\\Documents\\GitHub\\Ballistic-Deposition\\Deposition\\Ballistic-Deposition-with-Relaxation\\Fig\\log-log.png")
+=#
+
+Parameters = Dict(:len => 200,
+                    :tot_time => 6,
+                        :time_steps => 50)
+meanVar = [ 0.0 for i = 1:Parameters[:time_steps]]
+for i in 1:100
+    Time, VarList = Deposition(;Parameters...)
+    meanVar += VarList
+    print("\r$i")
+end
+VarList = meanVar / 100
+
+Time = exp.(0:Parameters[:tot_time]/(Parameters[:time_steps]-1):Parameters[:tot_time])
+Line, last_point = FindLine(Parameters[:time_steps], Time, VarList)
+X = 0:log(Time[last_point])
+Y = X .* Line[1] .+ Line[2]
+
+scatter(log.(Time),log.(VarList),
+    xlabel= L"Log\ Time",
+    ylabel= L"Log\ W_{(t)}",
+    title= L"Log-Log\ Plot\ of\ ~W_{(t)}-Time~",
+    label = L"Data\ point")
+
+plot!(X,Y,label = L"y = %$(round(Line[1],digits= 2))x + %$(round(Line[2],digits= 2))")
